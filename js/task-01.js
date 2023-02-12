8672e9d7139ae11cbe44a0b58b1c208ee6b54e06
Maciej Kudej <maciejkudej765@gmail.com>
2023-02-12 22:18:51 +0100

    Adding task2

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
 const categories = document.querySelectorAll("li.item");
 console.log(`Number of categories: ${categories.length}`);
 
-for (let i = 0; i < categories.length; i++){
-    console.log(' ');
-    const categoryName = categories[i].getElementsByTagName('h2');
-    console.log(`Category: ${categoryName[0].textContent}`);
+categories.forEach((category) => {
+  console.log(" ");
+  const categoryName = category.getElementsByTagName("h2");
+  console.log(`Category: ${categoryName[0].textContent}`);
 
-    const list = categories[i].getElementsByTagName('ul');
-    const listItems = list[0].getElementsByTagName("li");
-    console.log(`Elements: ${listItems.length}`);
-}
+  const list = category.getElementsByTagName("ul");
+  const listItems = list[0].getElementsByTagName("li");
+  console.log(`Elements: ${listItems.length}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = [];
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  ingredientsList.push(listItem);
+});
+
+console.log(ingredientsList);
+
+const list = document.querySelector("ul#ingredients");
+list.append(...ingredientsList);
+
