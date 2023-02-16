2c723368fda7a168b2137dd91807ba878f846aa2
Maciej Kudej <maciejkudej765@gmail.com>
2023-02-16 23:52:55 +0100

    Adding task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector("button.change-color");
+const backgroundSpan = document.querySelector("span.color");
+const body = document.querySelector("body");
+
+
+button.addEventListener("click", changeColor);
+
+
+function changeColor()  {
+  const color = getRandomHexColor();
+  body.setAttribute("style", `background-color:${color}`);
+  backgroundSpan.textContent = color;
+};
+
+
